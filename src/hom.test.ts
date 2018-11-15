de0eef4aebda17bdbedba5a71b9864771dc6581e
aaronamm <theeranitp@gmail.com>
2018-11-15 19:40:29 +0700

    initial setup to make a test pass

---
+++ src/hom.test.ts
@@ -0,0 +1,24 @@
+import * as puppeteer from 'puppeteer';
+
+describe('Jetabroad', () => {
+    beforeAll(done => {
+        jest.setTimeout(60000);
+        done();
+    });
+
+    it('should reach booking form', async () => {
+        const browser = await puppeteer.launch();
+        const context = await browser.createIncognitoBrowserContext();
+        const page = await context.newPage();
+
+        await page.goto('http://codesanook.com', {
+            waitUntil: 'domcontentloaded'
+        });
+
+        const bocy = await page.$('body');
+        expect(bocy).toBeDefined();
+
+        await context.close();
+        await browser.close();
+    });
+});
